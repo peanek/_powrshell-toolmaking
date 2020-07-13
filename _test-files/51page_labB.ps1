5ee711957697aa4b8dba3033605edd65fd696682
Przemysław Pęczkowski <ppeczkowski@live.com>
2020-07-13 16:52:46 +0200

    LabB

--- _test-files/51page_labB.ps1
+++ _test-files/51page_labB.ps1
@@ -1,15 +1,39 @@
-$logFile = 'c:\_logs\ErrorLog_52page_labC.log'
+# $logFile = 'c:\_logs\ErrorLog_52page_labC.log'
 
-function Get-RemoteComputerDiskInfo {
-    param (
-        [string]$computerName = 'localhost'
+# function Get-RemoteComputerDiskInfo {
+#     param (
+#         [string]$computerName = 'localhost'
+#     )
+
+#     $disks = Get-WmiObject -Class Win32_Volume -Filter 'DriveType=3'
+
+#     foreach ($disk in $disks) {
+#         $disk | Select ComputerName,Drive,FreeSpace,Size
+#     }
+# }
+
+# Get-RemoteComputerDiskInfo
+
+function Get-DiskInfo {
+    [CmdletBinding()]
+    param(
+        [string[]]$ComputerName="localhost",
+        [string]$ErrorLog
     )
 
-    $disks = Get-WmiObject -Class Win32_Volume -Filter 'DriveType=3'
-
-    foreach ($disk in $disks) {
-        $disk | Select ComputerName,Drive,FreeSpace,Size
+    BEGIN {
+        Write-Host "Error log is $ErrorLog"
     }
-}
+    PROCESS {
+        foreach ($Computer in $ComputerName) {
+            $drive = Get-WmiObject -Class Win32_DiskDrive `
+                -ComputerName $ComputerName
 
-Get-RemoteComputerDiskInfo
+            $properties = @{'FreeSpace'=$drive.}
+            
+        }
+    }
+    END{}
+
+}
+Get-DiskInfo -ErrorLog x.txt -ComputerName host1,host2,host3
